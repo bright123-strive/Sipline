9c7f545f3c125b7132955b8f08b98f6272898b9a
bright123-strive <brimuwemi@gmail.com>
2023-03-30 13:14:35 +0200

    #login2

--- core/login.php
+++ core/login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -49,76 +48,6 @@
 
                             <div>
                                 <input type="password" name="password" class="form-control" placeholder="Password">
-=======
-<?php
-
-		require_once ("sip_backend/login_backend.php");
-
-	?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>icall</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">    
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://www.markuptag.com/bootstrap/5/css/bootstrap.min.css">
-  <style>
-  #login-wrap{
-    position: absolute;
-    top: 22%;
-    left: 2%; 
-    height: 60%;
-    width: 30%;
-    padding:0px;
-    padding-top:0px;
-    margin-right:2%;
-    margin-left: 200px;
-    margin-top: 0px;
-    border: 0px solid #d1c7a3;
-    border-radius:3px;
-}
-.btn-xl {
-    padding: 2px 30px;
-    border-radius: 2px;
-}
-#intro{
-  background-image: url('images/image.png');
-  height: 100vh;
-  background-attachment: fixed;
-  background-position: right;
-  background-repeat:no-repeat;
-}
-</style>
-</head>
-<body>
-<div class="container" id="intro" style="padding-left:0px;">
-        <div class="row">
-            <div class="col-md-4 offset-md-4" id="login-wrap" style="border-radius:3px;">
-                <div class="login-form bg-light mt-4 p-4">
-                    <form action="" method="post" class="row g-3" autocomplete="off">
-                        <h4>iCall Login</h4>
-                        <div class="col-12">
-                            <label>Username</label>
-                            <input type="text" name ="email" class="form-control" placeholder="Username">
-                        </div>
-                        <div class="col-12">
-                            <label>Password</label>
-                            <input type="password" name="password1" class="form-control" placeholder="Password">
-                        </div>
-                        <br>
-                        <div class="col-12">
-                        <div class="row mb-4">
-                            <div class="col d-flex justify-content-center">
-                              <!-- Checkbox -->
-                              <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
-                                <label class="form-check-label" for="form1Example3">
-                                  Remember me
-                                </label>
-                              </div>
->>>>>>> c29f02f1da4d59a05306761a2fd2c4adfee4e5a4
                             </div>
 
                             <div class="col text-center">
